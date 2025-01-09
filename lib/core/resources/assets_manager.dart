269ae863d1717ba6fb13adceb109f0d007df8a8b
Mayar Khaled <mayarkhaled915@gmail.com>
2025-01-09 03:58:10 +0200

    Tasbeh tab is done

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -6,6 +6,12 @@ abstract final class AssetImages {
   static const String splashImage = '${base}splash_image.png';
   static const String mostRecentImage = '${base}most_recent_image.png';
   static const String mainBackground = '${base}main_bg.png';
+  static const String alsebhaHead = '${base}Alsebha_head.png';
+  static const String alsebhaBody = '${base}SebhaBody 1.png';
+  static const String womenReadingQuran = '${base}woman-reading-quran.png';
+  static const String alsebhaBackgound = '${base}AlsebhaBG1.png';
+  static const String soundWave = '${base}soundWave.png';
+  static const String mosques = '${base}mosques.png';
 }
 
 abstract final class SvgAssets {
@@ -17,9 +23,10 @@ abstract final class SvgAssets {
   static const String hadithIcon = '${base}hadith_icon.svg';
   static const String selectedQuranIcon = '${base}selected_quran_icon.svg';
   static const String selectedRadioIcon = '${base}selected_radio_icon.svg';
-  static const String selectedPrayTimesIcon =
-      '${base}selected_pray_times_icon.svg';
+  static const String selectedPrayTimesIcon = '${base}selected_pray_times_icon.svg';
   static const String selectedTasbehIcon = '${base}selected_tasbeh_icon.svg';
   static const String selectedHadithIcon = '${base}selected_hadith_icon.svg';
   static const String quranSearchIcon = '${base}quran_search_icon.svg';
+  static const String muteIcon = '${base}Vector.svg';
+  static const String volumeHigh = '${base}Volume High.svg';
 }
--- lib/presentation/screens/home/tabs/tasbeh/tasbeh_tab.dart
+++ lib/presentation/screens/home/tabs/tasbeh/tasbeh_tab.dart
@@ -1,12 +1,91 @@
 import 'package:flutter/material.dart';
+import '../../../../../core/resources/assets_manager.dart';
+import '../../../../../core/resources/colors_manager.dart';
 
-class TasbehTab extends StatelessWidget {
+class TasbehTab extends StatefulWidget {
   const TasbehTab({super.key});
 
   @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
+
+class _TasbehTabState extends State<TasbehTab> {
+  List<String> tasbehList = ['سبحان الله', 'الحمد لله', 'لا اله الا الله', 'الله اكبر'];
+  int _counter = 0;
+  int _currentTasbehIndex = 0;
+
+  void _incrementCounter() {
+    if (_counter < 30) {
+      setState(() {
+        _counter++;
+      });
+    } else {
+      setState(() {
+        _counter = 0;
+        _currentTasbehIndex = (_currentTasbehIndex + 1) % tasbehList.length;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.yellow,
+    return Stack(
+      children: [
+        Positioned.fill(
+          child: Image.asset(
+            AssetImages.alsebhaBackgound,
+            fit: BoxFit.cover,
+          ),
+        ),
+        Positioned.fill(
+          child: Container(
+            color: Colors.black.withOpacity(0.8),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Center(child: Image.asset(AssetImages.islamiLogo)),
+              const SizedBox(
+                height: 20,
+              ),
+              const Center(
+                child: Text(
+                  'سبح اسم ربك الاعلى',
+                  style: TextStyle(color: ColorsManager.white, fontSize: 36, fontWeight: FontWeight.bold),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Stack(
+                alignment: AlignmentDirectional.center,
+                children: [
+                  Positioned(top: 0.5, child: Image.asset(AssetImages.alsebhaHead)),
+                  Image.asset(AssetImages.alsebhaBody),
+                  InkWell(
+                    onTap: () {
+                      _incrementCounter();
+                    },
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          tasbehList[_currentTasbehIndex],
+                          style: const TextStyle(color: ColorsManager.white, fontSize: 28, fontWeight: FontWeight.bold),
+                        ),
+                        Text('$_counter', style: const TextStyle(color: ColorsManager.white, fontSize: 28, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
