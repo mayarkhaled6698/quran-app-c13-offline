415e4845c0fda960222b62b5d6e899c8af48b8b7
Mayar Khaled <mayarkhaled915@gmail.com>
2025-01-14 23:23:17 +0200

    Update most_recent_item.dart

--- lib/presentation/screens/home/tabs/quran/widgets/most_recent_item.dart
+++ lib/presentation/screens/home/tabs/quran/widgets/most_recent_item.dart
@@ -1,49 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:islami_c13_offline/core/resources/app_styles.dart';
 import 'package:islami_c13_offline/core/resources/assets_manager.dart';
 import 'package:islami_c13_offline/core/resources/colors_manager.dart';
+import 'package:islami_c13_offline/model/sura.dart';
 
-class MostRecentItem extends StatelessWidget {
-  const MostRecentItem({super.key});
+class MostRecentSurasWidget extends StatefulWidget {
+  const MostRecentSurasWidget({super.key});
+
+  @override
+  State<MostRecentSurasWidget> createState() => MostRecentSurasWidgetState();
+}
+
+class MostRecentSurasWidgetState extends State<MostRecentSurasWidget> {
+  List<Sura> mostRecentSuras = [];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getMostRecentSuras(); //
+  }
+
+  void getMostRecentSuras() async {
+    mostRecentSuras = await PrefsHandler.getMostRecentSuras();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
+    return mostRecentSuras.isEmpty
+        ? Container()
+        : SizedBox(
+            height: MediaQuery.of(context).size.height * 0.22,
+            //width: MediaQuery.of(context).size.width * 0.2,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Most Recently',
+                  style: AppStyles.whiteSugarBold16,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Expanded(
+                  child: ListView.builder(scrollDirection: Axis.horizontal, itemBuilder: (context, index) => mostRecentSuraWidget(mostRecentSuras[index]), itemCount: mostRecentSuras.length),
+                )
+              ],
+            ),
+          );
+  }
+
+  Widget mostRecentSuraWidget(Sura sura) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 10),
-      width: 283,
+      padding: EdgeInsets.all(12),
+      margin: EdgeInsets.symmetric(horizontal: 5),
       decoration: BoxDecoration(
-          color: ColorsManager.gold, borderRadius: BorderRadius.circular(20)),
+        borderRadius: BorderRadius.circular(12),
+        color: ColorsManager.gold,
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          const Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          Column(
             children: [
-              Text(
-                'Al-Fatiha',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ColorsManager.black60),
-              ),
-              Text(
-                'الفاتحه',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ColorsManager.black60),
-              ),
-              Text(
-                '7',
-                style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                    color: ColorsManager.black60),
-              ),
+              Expanded(
+                  child: Text(
+                sura.nameEn,
+                style: AppStyles.blackAcc700,
+              )),
+              Expanded(
+                  child: Text(
+                sura.snameAr,
+                style: AppStyles.blackAcc700,
+              )),
+              Expanded(
+                  child: Text(
+                '${sura.numberOfVerses} verses',
+                style: AppStyles.blackAcc700,
+              )),
             ],
           ),
-          Image.asset(AssetImages.mostRecentImage),
+          Image.asset(AssetImages.mostRecentImage)
         ],
       ),
     );
