8e48099c27e36e89431e0af6c77041bba7211931
Mayar Khaled <mayarkhaled915@gmail.com>
2025-01-14 23:11:54 +0200

    Update app_styles.dart

--- lib/core/resources/app_styles.dart
+++ lib/core/resources/app_styles.dart
@@ -7,4 +7,40 @@ class AppStyles {
     fontWeight: FontWeight.bold,
     color: ColorsManager.whiteSugar,
   );
+  static const TextStyle whiteBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.white,
+  );
+  static const TextStyle goldBold20 = TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.gold,
+  );
+  static const TextStyle goldBold16 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.gold,
+  );
+  static const TextStyle goldBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.gold,
+  );
+  static const TextStyle blackBold24 = TextStyle(
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.black,
+  );
+  static const TextStyle whiteBold14 = TextStyle(
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.white,
+  );
+  static const TextStyle whiteBold12 = TextStyle(
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    color: ColorsManager.white,
+  );
+  static const TextStyle blackAcc700 = TextStyle(fontWeight: FontWeight.w700, fontSize: 24, color: ColorsManager.blackAcc);
 }
