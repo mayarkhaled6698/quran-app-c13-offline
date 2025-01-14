879ed22d5dd5f293376c8af1fe95e3d19e6a9760
Mayar Khaled <mayarkhaled915@gmail.com>
2025-01-14 23:05:31 +0200

    Update assets_manager.dart

--- lib/core/resources/assets_manager.dart
+++ lib/core/resources/assets_manager.dart
@@ -6,6 +6,22 @@ abstract final class AssetImages {
   static const String splashImage = '${base}splash_image.png';
   static const String mostRecentImage = '${base}most_recent_image.png';
   static const String mainBackground = '${base}main_bg.png';
+  static const String alsebhaHead = '${base}Alsebha_head.png';
+  static const String alsebhaBody = '${base}SebhaBody 1.png';
+  static const String womenReadingQuran = '${base}woman-reading-quran.png';
+  static const String alsebhaBackgound = '${base}AlsebhaBG1.png';
+  static const String soundWave = '${base}sound_wave.png';
+  static const String mosques = '${base}Mosque.png';
+  static const String islamicShapeLeft = '${base}islamic_shape_left.png';
+  static const String islamicShapeRight = '${base}islamic_shape_right.png';
+  static const String hadithMainBackground = '${base}hadith_main_bg.png';
+  static const String quranBottomImage = '${base}quran_bottom_image.png';
+  static const String hadithCardBg = '${base}hadith_card_bg.png';
+  static const String onboarding1 = '${base}onboarding1.png';
+  static const String onboarding2 = '${base}onboarding2.png';
+  static const String onboarding3 = '${base}onboarding3.png';
+  static const String onboarding4 = '${base}onboarding4.png';
+  static const String onboarding5 = '${base}onboarding5.png';
 }
 
 abstract final class SvgAssets {
@@ -17,9 +33,12 @@ abstract final class SvgAssets {
   static const String hadithIcon = '${base}hadith_icon.svg';
   static const String selectedQuranIcon = '${base}selected_quran_icon.svg';
   static const String selectedRadioIcon = '${base}selected_radio_icon.svg';
-  static const String selectedPrayTimesIcon =
-      '${base}selected_pray_times_icon.svg';
+  static const String selectedPrayTimesIcon = '${base}selected_pray_times_icon.svg';
   static const String selectedTasbehIcon = '${base}selected_tasbeh_icon.svg';
   static const String selectedHadithIcon = '${base}selected_hadith_icon.svg';
   static const String quranSearchIcon = '${base}quran_search_icon.svg';
+  static const String muteIcon = '${base}Vector.svg';
+  static const String unmute = '${base}unmute.svg';
+  static const String playIcon = '${base}Play.svg';
+  static const String pauseIcon = '${base}PauseIcon.svg';
 }
