6fe7cb312e14a5f5f3f98480ed1c8215edb75c80
Mayar Khaled <mayarkhaled915@gmail.com>
2025-01-14 23:35:27 +0200

    Update routes_manager.dart

--- lib/core/routes_manager/routes_manager.dart
+++ lib/core/routes_manager/routes_manager.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:islami_c13_offline/presentation/screens/home/home.dart';
+import 'package:islami_c13_offline/presentation/screens/onboarding/onboarding.dart';
 import 'package:islami_c13_offline/presentation/screens/splash/splash.dart';
+import 'package:islami_c13_offline/presentation/screens/sura_details_screen/sura_details_screen.dart';
 
 class RoutesManager {
   static const String splash = '/splash';
   static const String home = '/home';
+  static const String suraDetails = '/suraDetails';
+  static const String onboarding = '/onboarding';
 
   static Map<String, WidgetBuilder> routes = {
     splash: (_) => const Splash(),
     home: (_) => const Home(),
+    suraDetails: (_) => const SuraDetailsScreen(),
+    onboarding: (_) => Onboarding(),
   };
 }
